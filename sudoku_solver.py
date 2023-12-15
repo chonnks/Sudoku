d0fe11788d6ca3ad7301d823fa0890990e150e17
chonnks <136377272+chonnks@users.noreply.github.com>
2023-12-15 01:53:35 -0500

    Add files via upload
    finished product without implementation of checklist items yet

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,8 +1,8 @@
 #Bruce Reyes
 #INST126
 #This is a sudoku puzzle solver
-#i used documentation from stackoverflow and computerphile's video 
-# but every part of the code can and will be explained
+#I used documentation from stackoverflow and computerphile's video 
+#but every part of the code can and will be explained
 
 #grid for sudoku: 9x9  with prefilled number
 SudokuGrid = [ #this is our starting board
@@ -16,6 +16,53 @@ SudokuGrid = [ #this is our starting board
     [1, 2, 0, 0, 0, 7, 4, 0 ,0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+#algorithm that actually solves the sudoku board
+def SudokuSolve(grid):
+    FindSolution = FindSquare(grid)
+    if not FindSolution: 
+        return True # means that we found solution
+    else:
+        row, column = FindSolution
+    
+    for i in range (1,10): #excludes zero and goes 1-9
+        if ValidBoard(grid, i, (row, column)): # if we add them into the board, they will be valid
+            grid[row][column] = i
+
+            if SudokuSolve(grid):
+                return True
+            
+            grid[row][column] = 0 #backtracks  to last element and tries with different element 
+    
+    return False    
+
+
+#function to find if current board is valid in terms of sudoku
+def ValidBoard(grid, number, position):
+    #checking row using loops
+    for i in range(len(grid[0])):
+        if grid[position[0]][i] == number and position[1] != i: #scan if number is in row, if it is just avoid that row 
+            return False
+
+    #checking column using same loop
+    for i in range(len(grid[0])):
+        if grid[i][position[1]] == number and position[0] != i: #scan if number is in column, if it is just avoid that column
+            return False
+    
+    # Check the 3x3 box
+    # allows each box to have a specific coordinate (0,0), (0,1) etc.
+    BoxRow = position[1] // 3
+    BoxColumn = position[0] // 3
+
+    #loop through all elements in box
+    for i in range(BoxColumn * 3, BoxColumn * 3 + 3): 
+        for j in range(BoxRow * 3, BoxRow * 3 + 3): #this find what box we're in and multiplies to get to index values
+            if grid[i][j] == number and (i,j) != position: # if number is already found in box, we do not have to go into box
+                return False
+
+    return True
+
+
 
 # function that actually prints out grid
 def PrintGrid(grid):
@@ -41,7 +88,13 @@ def FindSquare(grid):
             if grid[i][j] == 0:
                 return (i,j) # row and column in empty square
 
+    return None #if there are no zeroes then it will return FindSolution
 
+print("Non-Solved Board\n")
+PrintGrid(SudokuGrid) # prints before solving
+SudokuSolve(SudokuGrid)
+print("Solved Board\n")
+PrintGrid(SudokuGrid) #prints after to show before and after
 
 
     
